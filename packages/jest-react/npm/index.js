f572b4ed55aa24116584e3dd04d39bcaa18aa316
gimmy <975402925@qq.com>
2024-03-10 14:29:10 +0800

    test

--- packages/jest-react/npm/index.js
+++ packages/jest-react/npm/index.js
@@ -2,6 +2,7 @@
 
 if (process.env.NODE_ENV === 'production') {
   module.exports = require('./cjs/jest-react.production.min.js');
+  console.log(123)
 } else {
   module.exports = require('./cjs/jest-react.development.js');
 }
